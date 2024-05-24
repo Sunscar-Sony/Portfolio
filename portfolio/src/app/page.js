98c6d207ea28b912d047f00777aded1eac52361e
Sanskar Soni <sunscarsony@gmail.com>
2024-05-24 16:35:05 +0530

    File Pushed

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -2,6 +2,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1 className={styles.main}>Hello World!</h1>
+    <h1 className={styles.card}>Hello World!</h1>
   );
 }
