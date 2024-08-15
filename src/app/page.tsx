a2515a5de1d5d1a9a7249855c220229a789f7aab
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-08-15 12:09:55 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,1 @@
-import Image from "next/image";
-import {HeroSection} from "../Components/HeroSection";
 
-export default function Home() {
-  return (
-    <>
-    <HeroSection />
-    </>
-  );
-}
