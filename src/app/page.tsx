0d1cd548dd2a5c125cf5225470916581eb339d79
Sunscar-Sony <166742286+Sunscar-Sony@users.noreply.github.com>
2024-09-29 20:54:11 +0530

    Notification Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import {HeroSection} from "../Components/HeroSection";
+import NavbarDemo from "../Components/Navbar";
 
 export default function Home() {
   return (
     <>
+    <NavbarDemo />
     <HeroSection />
     </>
   );
